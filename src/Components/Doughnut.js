fd5a7f7e32ed4d0dcb2e884ab3ac77a953a6703d
EDWARDWEE-um <edwardwee11@gmail.com>
2021-04-22 22:20:16 +0800

    zenitsu

--- src/Components/Doughnut.js
+++ src/Components/Doughnut.js
@@ -75,7 +75,7 @@ export default function DoughnutChart() {
     const bull = <span className={classes.bullet}>•</span>;
     return (
         <div>
-        <Card className={classes.root} style={{}} >
+        <Card className={classes.root} style={{width:'23rem' , marginLeft:'auto', marginRight:'auto',marginTop:'1rem'}} >
         <CardContent>
         <div className='header'>
                 <h1 className='title'> Spending </h1>
